b18416bc3568a8fc32dcd8e6f7b8146ac2bd5e46
unknown <Administrator@PWC-QA-jhliu.(none)>
2016-09-07 20:10:42 +0800

    Finished all tasks of parkingLot but have not refact yet

--- ParkingLot/Study/ParkingLot.cs
+++ ParkingLot/Study/ParkingLot.cs
@@ -3,39 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace ParkingLotSystem
 {
      
     public class ParkingLot
-    {
-        public static int flag = 0;
-        public string park(string car)
-        {
-
-            if (car == "paoche")
-            {
-                flag = 1;
-                return "paoche";
-            }
-
-            else
-                return "qq";
-
+    {    
+        public static int counts = 0;
+        public static int parkingLotMax = 0;
+        public Hashtable parkingLotList = new Hashtable();
+     
+    
+        public ParkingLot(int i)
+        {                      
+            parkingLotMax = i;                      
+ 
         }
-        public string pick(string key)
+        public ParkingLot()
+        {
+            parkingLotMax = 100;
+        }
+        public int park(string car)
         {
 
-            if (key == "paoche" && flag == 1)
+            if (counts < parkingLotMax)
             {
-                flag = 0;
-                return "paoche";
-
+                parkingLotList.Add(counts,car);
+                int key = counts++;                
+                return key;                
             }
-            else if (key == "qq")
-                return "qq";
             else
-                return null;
+                return 0;
+         }
+        public string pick(int key)
+        {
+
+           string result = (string)parkingLotList[key];
+           if (result!=null)
+           {               
+               parkingLotList.Remove(key);
+               counts--;
+               return result;
+           }            
+           else
+               return null;
+
+            
            
         }
         
--- ParkingLot/Test/TestParkingLot.cs
+++ ParkingLot/Test/TestParkingLot.cs
@@ -13,21 +13,20 @@ namespace TestParkingLotSystem
         [Fact]
         public void should_pick_one_car_when_pick_a_parked_car()
         {
-            ParkingLot parkingLot = new ParkingLot();
+            ParkingLot parkingLot = new ParkingLot(2);
             
-            string key = parkingLot.park("paoche");
+            int key = parkingLot.park("paoche");
         
             Assert.Same("paoche", parkingLot.pick(key));
         }
 
-
         [Fact]
         public void should_pick_correct_car_when_pick_car_with_parked_multi_cars()
         {
-            ParkingLot parkingLot = new ParkingLot();
+            ParkingLot parkingLot = new ParkingLot(2);
 
-            string key1 = parkingLot.park("paoche");
-            string key2 = parkingLot.park("qq");
+            int key1 = parkingLot.park("paoche");
+            int key2 = parkingLot.park("qq");
 
             Assert.Same("paoche", parkingLot.pick(key1));
             Assert.Same("qq", parkingLot.pick(key2));
@@ -36,21 +35,34 @@ namespace TestParkingLotSystem
         [Fact]
         public void should_not_pick_the_car_repeatly_when_pick_a_parked_car()
         {
-            string key;
-            ParkingLot parkingLot = new ParkingLot();
+            int key;
+            ParkingLot parkingLot = new ParkingLot(2);
 
-            key = parkingLot.park("paoche");
+            key = parkingLot.park("BMW");
             parkingLot.pick(key);
             Assert.Null(parkingLot.pick(key));
         }
 
-        public void should_not_park_a_car_when_parking()
-        { 
+        [Fact]
+        public void should_not_park_a_car_when_parkingLot_is_full()
+        {
+            ParkingLot parkingLot = new ParkingLot(2);
+            parkingLot.park("W");
+            parkingLot.park("Z");
+            Assert.Equal(0,parkingLot.park("H"));
+
         }
+        [Fact]
+        public void should_be_able_to_park_a_car_after_pick_a_car_from_full_parkingLot()
+        {
+            ParkingLot parkingLot = new ParkingLot(1);
+            int key =  parkingLot.park("W");
+            parkingLot.pick(key);
 
+            int key2 = parkingLot.park("Z");
+            Assert.Same("Z",parkingLot.pick(key2));
 
-
-        
+        }
         
     }
 }
